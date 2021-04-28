6a01ad1b6a051f292c5bd746fcc9e7d8dd931e80
Willie <write2willie@gmail.com>
2021-04-28 16:50:13 -0400

    commit #10: added post route and tested it, april282021, 4:50pm

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const fs = require('fs');
 
 //json file
 let db = require('./db/db.json');
+const { text } = require('express');
 
 //define a port to listen for incoming requests.
 const PORT = 3000;
@@ -19,18 +20,22 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//middleware 3
+//middleware 3.
 //when a request comes in for a static file, the express.static 
 //middleware points the request to the proper folder where the 
 //static asset can be found. Credit: TA Adam Hartleb (udemy).
-//__dirname returns the current path of the application.
+//__dirname returns the current path of the application or the 
+//directory that the currently executing script is in.
 //static middleware serving files from the public directory to the client.
 app.use(express.static(__dirname + '/public')); 
 // app.use(express(__dirname + '/public')); 
+//console.log("dirname " + __dirname)
 
 
 //this is the first html route (root route): http://127.0.0.1:3000. 
 //it uses the GET verb of the app object (express).
+//express provides a method in the response object (res in our case)  
+//of the router called sendFile() that can be used to serve static files. 
 app.get('/', (req, res) => {
     //the path.join() method joins all given path segments 
     //together using the platform-specific separator as a delimiter.
@@ -49,7 +54,25 @@ app.get(`/api/notes`, (req, res) => {
     res.json(db);
 });
 
-//test by sending a string to the client when the user visits the PORT URL.
+app.post('/api/notes', (req, res) => {
+        //adds the new entry into the db.json file (in mem)
+        db.push(req.body);
+    
+        //adds the new entry into the db.json file (into the physical db.json file)
+        fs.writeFile(path.join(__dirname, './db/db.json'), JSON.stringify(db), (err) => {
+            if (err) {
+                throw err;
+            }
+            });
+
+            //returns db.json with new content
+            res.json(db);
+        
+     });
+
+
+//inform the client on which port this app listens on; throw an
+//error is something goes wrong.
 app.listen(PORT, (err) => {
     if (err) {
         console.log(err);
